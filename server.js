eca3201ad3c6ffbe2f0dc8c5dfc81fa10d29213a
gldaniil <daniil.gajfullin@yandex.ru>
2024-02-02 00:55:50 +0500

    Выборка и преобразование данных

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const createDB = require("./scripts/createDB");
 const setData = require("./scripts/setData");
 const getUrls = require("./scripts/getUrls");
 const tempSetCode = require("./scripts/tempSetCode");
+const tempGetCode = require("./scripts/tempGetCode");
 
 const dirDB = "./data/" + process.env.DB_NAME;
 
@@ -28,8 +29,46 @@ async function request(url) {
 (async () => {
   getUrls(dirDB, (urls) => {
     urls.forEach(async (url) => {
-      const result = await request(url);
-      tempSetCode(dirDB, result);
+      // const code = await request(url);
+      // tempSetCode(dirDB, url, code);
+      tempGetCode(dirDB, url, (code) => {
+        const arr = [];
+        const keys = ["number", "date", "solution"];
+        const dom = new JSDOM(code);
+        const courtName = dom.window.document.querySelector("h5").textContent;
+        console.log(courtName);
+        const table = dom.window.document
+          .querySelector("table#tablcont")
+          .querySelector("tbody");
+        // Проход по каждой строке таблицы
+        for (const row of table.children) {
+          // Только перечень дел
+          if (row.vAlign) {
+            // Выборка дел, начинающихся с `2-`
+            if (row.children[0].textContent.includes("2-")) {
+              const [numberCell, dateCell, categoryCell] = row.children;
+              const splittedCategory = categoryCell.innerHTML.split("<br>");
+
+              const conversion = (str) => str.replace(/[\n\t]/g, "").trim();
+
+              const getPerson = (value) =>
+                splittedCategory
+                  .filter((str) => (str.includes(value) ? str : ""))
+                  .join("");
+              // Объект для дальнейшей загрузки в БД
+              const obj = {
+                number: numberCell.textContent,
+                date: conversion(dateCell.textContent),
+                plaintiff: conversion(getPerson("ИСТЕЦ")),
+                defendant: conversion(getPerson("ОТВЕТЧИК")),
+              };
+
+              arr.push(obj);
+            }
+          }
+        }
+        console.log(arr);
+      });
     });
   });
 })();
