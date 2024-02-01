2b9e0d86b1b4d14529cd2edf5ccce7f1a3e7a38a
gldaniil <daniil.gajfullin@yandex.ru>
2024-02-01 23:49:51 +0500

    Передача свойства объекта row

--- scripts/tempGetCode.js
+++ scripts/tempGetCode.js
@@ -1,12 +1,12 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const tempGetCode = (dirDB, url) => {
+const tempGetCode = (dirDB, url, callback) => {
   const db = new sqlite3.Database(dirDB);
   try {
     const query = "SELECT code FROM code WHERE url = ?";
     db.get(query, [url], (err, row) => {
       if (err) return console.error(err.message);
-      console.log(row);
+      callback(row.code);
     });
   } catch (e) {
     console.log(e);
