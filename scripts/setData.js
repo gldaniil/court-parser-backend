26b32f8635c58fca5f6077e57d5c5d21eab2fa4a
gldaniil <daniil.gajfullin@yandex.ru>
2024-02-03 23:19:54 +0500

    Обновление данных таблицы urls после начала парсинга

--- scripts/setData.js
+++ scripts/setData.js
@@ -1,12 +1,31 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const setData = (dirDB) => {
+const setData = (dirDB, id, court_name, last_changed, courtName, now) => {
   const db = new sqlite3.Database(dirDB);
   try {
-    const query = db.prepare("INSERT INTO urls VALUES(NULL,?,?)");
-    query.run("https://example.com", "Example");
-    query.finalize();
-    db.close();
+    if (courtName !== court_name) {
+      const query = `UPDATE urls 
+        SET court_name = ?, last_changed = ?
+        WHERE id = ?`;
+      db.run(query, [courtName, now, id], (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          db.close();
+        }
+      });
+    } else {
+      const query = `UPDATE urls 
+        SET last_changed = ?
+        WHERE id = ?`;
+      db.run(query, [now, id], (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          db.close();
+        }
+      });
+    }
   } catch (e) {
     console.log(e);
   }
